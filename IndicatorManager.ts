79ac7a28b653d2b193c41c8b15f8a74f44e917ae
deepakrana32 <deepak.rana32@gmail.com>
2025-05-31 20:46:55 +0530

    IndicatorManager.ts

--- IndicatorManager.ts
+++ IndicatorManager.ts
@@ -1,43 +1,41 @@
-import { ChartPlugins } from './ChartPlugins';
-import { ChartRenderer } from './ChartRenderer';
-import { Candle } from './PatternTypes';
+import { IndicatorRenderer } from './IndicatorRenderer';
+import { Candle } from './ChartTypes';
 
 export class IndicatorManager {
-  private plugins: ChartPlugins;
-  private renderer: ChartRenderer;
-  private activeIndicators: string[];
+  private renderer: IndicatorRenderer;
+  private activeIndicators: { id: string; type: string; params?: any }[];
 
-  constructor(renderer: ChartRenderer) {
+  constructor(renderer: IndicatorRenderer) {
     if (!renderer) throw new Error('Renderer missing');
-    this.plugins = new ChartPlugins();
     this.renderer = renderer;
     this.activeIndicators = [];
-    this.plugins.setRenderer(this.renderer);
   }
 
-  addIndicator(id: string) {
-    if (!this.activeIndicators.includes(id)) {
-      this.activeIndicators.push(id);
+  addIndicator(id: string, type: string, params?: any) {
+    if (!this.activeIndicators.find(i => i.id === id)) {
+      this.activeIndicators.push({ id, type, params });
+      this.renderer.addIndicator(id, type, params);
     }
   }
 
   removeIndicator(id: string) {
-    this.activeIndicators = this.activeIndicators.filter(i => i !== id);
+    this.activeIndicators = this.activeIndicators.filter(i => i.id !== id);
+    this.renderer.removeIndicator(id);
   }
 
   updateIndicators(candles: Candle[]) {
     const closes = new Float32Array(candles.map(c => c.close));
     const highs = new Float32Array(candles.map(c => c.high));
     const lows = new Float32Array(candles.map(c => c.low));
-    this.plugins.computeIndicators(closes, highs, lows);
+    this.renderer.computeIndicators(this.activeIndicators, closes, highs, lows);
   }
 
   render(ctx: CanvasRenderingContext2D) {
-    this.plugins.render2D(ctx);
+    this.renderer.render2D(ctx);
   }
 
   destroy() {
-    this.plugins.destroy();
+    this.renderer.destroy();
     this.activeIndicators = [];
   }
 }
